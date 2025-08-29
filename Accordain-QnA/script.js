03e72fd56d1ad88077efc2b86a555ee33fe06efa
Your Name <43210sbk@gmail.com>
2025-08-29 17:11:48 +0530

    QnA not working

---
+++ Accordain-QnA/script.js
@@ -0,0 +1,67 @@
+let questionOne = document.querySelector('#qns1');
+let questionTwo = document.querySelector('#qns2');
+let questionThree = document.querySelector('#qns3');
+let questionFour = document.querySelector('#qns4');
+let questionFive = document.querySelector('#qns5');
+
+let answerOne = document.querySelector('#show-ans1');
+let answerTwo = document.querySelector('#show-ans2');
+let answerThree = document.querySelector('#show-ans3');
+let answerFour = document.querySelector('#show-ans4');
+let answerFive = document.querySelector('#show-ans5');
+
+let closeCrossOne = document.querySelector('#closeBtn1');
+let closeCrossTwo = document.querySelector('#closeBtn2');
+let closeCrossThree = document.querySelector('#closeBtn3');
+let closeCrossFour = document.querySelector('#closeBtn4');
+let closeCrossFive = document.querySelector('#closeBtn5');
+
+// let answerDisplay = document.querySelector('.answers');
+let overlayLayer = document.querySelector('.overlay-layer');
+
+questionOne.addEventListener('click', () => {
+  answerOne.style.display = 'flex';
+  questionOne.style.zIndex = '20';
+  overlayLayer.style.visibility = 'visible';
+});
+closeCrossOne.addEventListener('click', () => {
+  answerOne.style.display = 'none';
+  overlayLayer.style.visibility = 'hidden';
+});
+
+questionTwo.addEventListener('click', () => {
+  answerTwo.style.display = 'flex';
+  questionTwo.style.zIndex = '20';
+  overlayLayer.style.visibility = 'visible';
+});
+closeCrossTwo.addEventListener('click', () => {
+  answerTwo.style.display = 'none';
+  overlayLayer.style.visibility = 'hidden';
+});
+questionThree.addEventListener('click', () => {
+  answerThree.style.display = 'flex';
+  questionThree.style.zIndex = '20';
+  overlayLayer.style.visibility = 'visible';
+});
+closeCrossThree.addEventListener('click', () => {
+  answerThree.style.display = 'none';
+  overlayLayer.style.visibility = 'hidden';
+});
+questionFour.addEventListener('click', () => {
+  answerFour.style.display = 'flex';
+  questionFour.style.zIndex = '20';
+  overlayLayer.style.visibility = 'visible';
+});
+closeCrossFour.addEventListener('click', () => {
+  answerFour.style.display = 'none';
+  overlayLayer.style.visibility = 'hidden';
+});
+questionFive.addEventListener('click', () => {
+  answerFive.style.display = 'flex';
+  questionFive.style.zIndex = '20';
+  overlayLayer.style.visibility = 'visible';
+});
+closeCrossFive.addEventListener('click', () => {
+  answerFive.style.display = 'none';
+  overlayLayer.style.visibility = 'hidden';
+});
